289308a250cd89ccc276c5298ae9d01a906a05d0
SALLAH-JP <jeanpaulsallah@gmail.com>
2025-04-22 14:53:46 +0400

    tt

--- turtle_regulation/turtle_regulation/set_way_point.py
+++ turtle_regulation/turtle_regulation/set_way_point.py
@@ -10,12 +10,13 @@ class MyPublisherNode(Node):
     def __init__(self):
         super().__init__("first_pub")
         self.publisher_=self.create_publisher(Twist, "/turtle1/cmd_vel", 10)
+        self.publisher_moving=self.create_publisher(Twist, "/turtle1/is_moving", 10)
         self.subscriber_=self.create_subscription(Pose, "/turtle1/pose", self.sub_callback, 10)
         self.create_timer(0.5, self.publisher_callback)
         self.x_wp = 7
         self.y_wp = 7
         self.kp = 2
-        self.kpl = 1
+        self.kpl = -1
         self.turtle_pose=Pose()
         self.get_logger().info("publisher has started")
        
@@ -30,16 +31,13 @@ class MyPublisherNode(Node):
         theta_turtle = self.turtle_pose.theta
         distance_tolerance = 0.5
         distance = self.distance()
-        self.get_logger().info(f'Valeur actuelle : {distance}')
-        if distance < distance_tolerance:
 
-            rep.linear.x += 1.0
-            rep.linear.y += 1.0
+        if distance > distance_tolerance:
+
+            rep.linear.x = self.kpl * distance
             rep.angular.z = self.commande_cap(theta_desired, theta_turtle) * self.kp
             self.publisher_.publish(rep)
-            return
 
-        self.publisher_.publish(rep)
         
 
     def angle_desired(self):
@@ -52,7 +50,8 @@ class MyPublisherNode(Node):
         return e
 
     def distance(self):
-        v = math.sqrt((self.turtle_pose.x - self.x_wp) ** 2 + (self.turtle_pose.y - self.y_wp) ** 2) * self.kpl
+        e1 = math.sqrt((self.turtle_pose.x - self.x_wp) ** 2 + (self.turtle_pose.y - self.y_wp) ** 2)
+        v = e1
         return v
 
         
